ff243cd04f2fbbe8998e8381e6208f62ef0823a2
Dmitri <DRU@popesco.com>
2014-06-09 00:02:19 +0300

    readme

--- config/defines.php
+++ config/defines.php
@@ -2,4 +2,5 @@
 
 define('PATH_APP', dirname(__DIR__). DIRECTORY_SEPARATOR . 'app/');
 define('PATH_LIBS', dirname(__DIR__). DIRECTORY_SEPARATOR . 'libs/');
+define('PATH_PUBLIC', dirname(__DIR__). DIRECTORY_SEPARATOR . 'public/');
 define('PATH_CONF', realpath(__DIR__). DIRECTORY_SEPARATOR);
