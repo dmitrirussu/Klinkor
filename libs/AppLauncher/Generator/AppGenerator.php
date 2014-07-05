09d9eb4c1f2d374ca5b9d5028968ac2b686a9b30
Dmitri <DRU@popesco.com>
2014-07-05 22:52:33 +0300

    was improved app generator

--- libs/AppLauncher/Generator/AppGenerator.php
+++ libs/AppLauncher/Generator/AppGenerator.php
@@ -262,17 +262,10 @@ class AppGenerator {
 <head>
 	<meta charset=\"UTF-8\">
 	<title>{$this->appName}</title>
-	<?php if (\$cssFiles): ?>
-		<?php foreach(\$cssFiles AS \$file): ?>
-			<link rel=\"stylesheet\" type=\"text/css\" href=\"/public/global/css/<?php echo(\$file); ?>.css\" />
-		<?php endforeach; ?>
-	<?php endif; ?>
-
-<?php if (\$javaScriptFiles): ?>
-	<?php foreach(\$javaScriptFiles AS \$file): ?>
-		<script type=\"text/javascript\" language=\"javascript\" src=\"/public/global/js/<?php echo(\$file); ?>.js\"></script>
-	<?php endforeach; ?>
-<?php endif; ?>
+	<?php
+		\\AppLauncher\\Scripts::showCSS();
+		\\AppLauncher\\Scripts::showJs();
+	?>
 </head>
 <body>
 <div class=\"header\"></div>
