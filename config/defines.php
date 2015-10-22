37f94dbf47af04f3ec1232c7452dab9b7bb2e46d
Dmitri <DRU@popesco.com>
2015-10-22 18:37:38 +0300

    update

--- config/defines.php
+++ config/defines.php
@@ -6,7 +6,14 @@ define('PATH_PRIVATE', dirname(__DIR__). DIRECTORY_SEPARATOR . 'private/');
 define('PATH_PUBLIC', dirname(__DIR__). DIRECTORY_SEPARATOR . 'public/');
 define('PATH_CONF', realpath(__DIR__). DIRECTORY_SEPARATOR);
 
-define('APP_FOLDER', str_replace('\\', '', str_replace(str_replace('/', '\\', $_SERVER['DOCUMENT_ROOT']), '', str_replace('/', '\\', dirname(__DIR__).DIRECTORY_SEPARATOR))));
+define('APP_FOLDER',
+	trim(str_replace('\\', '/',
+		str_replace(
+			str_replace('/', '\\', $_SERVER['DOCUMENT_ROOT']), '',
+			str_replace('/', '\\', dirname(__DIR__).DIRECTORY_SEPARATOR)
+		)
+	), '/')
+);
 
 $hostName = '//localhost';
 
