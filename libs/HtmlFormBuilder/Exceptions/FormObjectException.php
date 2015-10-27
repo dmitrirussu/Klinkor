dffccef14d7e21a5eb242690d4a93e3e0ef07630
Dmitri <DRU@popesco.com>
2015-10-27 16:57:39 +0200

    update

---
+++ libs/HtmlFormBuilder/Exceptions/FormObjectException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Dumitru Russu.
+ * Date: 12.09.2014
+ * Time: 12:39
+ * HtmlFormBuilder\Exceptions${NAME} 
+ */
+
+namespace HtmlFormBuilder\Exceptions;
+
+
+class FormObjectException extends \Exception {
+
+} 
